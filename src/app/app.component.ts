4f3d893b7b9ad945a4a41208a16eeeffd0e4aa5f
bciss <cissback@hotmail.fr>
2018-09-23 14:33:19 +0200

    searchbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PlantInfosService} from './services/plant-infos.service';
 
 @Component({
@@ -6,10 +6,16 @@ import {PlantInfosService} from './services/plant-infos.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Platation Helper';
 
-  constructor(private plantInfosService: PlantInfosService) {
+  constructor(private plantInfosService: PlantInfosService) {}
 
+  ngOnInit() {}
+
+  searchPlant (p_name: string) {
+    this.plantInfosService.getPlantInfos(p_name).then(value => {
+      console.log(value);
+    });
   }
 }
